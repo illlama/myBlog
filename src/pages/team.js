17ccd2bba854d52f113213d60a578a2a39a13074
illlama <beatlemaniakih@gmail.com>
2020-03-03 14:43:12 +0900

    added Bootstrap, ReactStrap and plugins about scss + created NavBar

---
+++ src/pages/team.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Team" />
+    Our team ...
+  </Layout>
+)
+
+export default TeamPage
